72b2eb204e9a9fa65cea40e6d60caea67cd6c637
Snowding Zerland <181250011@smail.nju.edu.cn>
2020-07-28 17:09:05 +0800

    dev: 采用分组rank修改codeLearn,添加显示用户能力图stdEva

--- codeLearn.py
+++ codeLearn.py
@@ -1,5 +1,6 @@
 import json
 import random
+import stdEva
 
 # 预设值
 USER_FILE = 'user.json'
@@ -34,8 +35,8 @@ def updateUserData():
 def addAccount(name):
 	evaluate = []
 	for i in CASE_TYPES:
-		evaluate.append({i: 0})
-	DATA_USERS[name] = {'user_name': name, 'rank_score': INIT_SCORE,'type_evaluate':evaluate, 'records': []}
+		evaluate.append({i:{'offset':0,'evaluate':0,'rank':0} })
+	DATA_USERS[name] = {'user_name': name, 'rank_score': INIT_SCORE,'type_info':evaluate, 'records': []}
 	updateUserData()
 
 
@@ -64,7 +65,8 @@ def login():
 
 
 def getRecommendCase(type, offset):
-	base = DATA_USERS[USER_NAME]['rank_score'] + offset
+	score = DATA_USERS[USER_NAME]['type_info'][CASE_TYPES[type]]['rank']
+	base = score + offset
 	has_done_case = list(map(lambda x: x['case_id'], DATA_USERS[USER_NAME]['records']))
 	enabled_cases = list(filter(lambda x: x['case_id'] not in has_done_case, filter(lambda x: x['case_type'] == CASE_TYPES[type],DATA_CASES)))
 	scoped_cases = list(filter(lambda x: base - RANK_SCOPE< x['case_score'] < base + RANK_SCOPE, enabled_cases))
@@ -96,19 +98,19 @@ def getRecommendCase(type, offset):
 	return res_cases
 
 
-def getOffset(evaluate):
-	return 0
+def getOffset(type):
+	return RANK_SCOPE*(DATA_USERS[USER_NAME]['type_info'][CASE_TYPES[type]]['offset']-0.5)
 
 
 def test():
 	print("测试题目难度会根据你的分数确定，并会采取计时来综合评估你的成绩，请注意做题时间")
 	input("已准备好按Enter键即可开始")
 	random_type = int(random.random()*len(CASE_TYPES))
-	evaluate = DATA_USERS[USER_NAME]['type_evaluates'][CASE_TYPES[random_type]]
-	case = getRecommendCase(random_type, getOffset(evaluate))[0]
+	case = getRecommendCase(random_type, getOffset(random_type))[0]
 	print("测试题目：")
 	print("     种类："+case['case_type'])
 	print("     下载地址："+case['case_zip'])
+	
 	pass
 
 
@@ -118,13 +120,13 @@ def exercise():
 	print("可选种类：1.字符串, 2.线性表, 3.数组, 4.查找算法，6.排序算法 5.数字操作, 7.树结构, 8图结构.")
 	type = int(input("请输入对应编号（1-8）："))-1
 	
-	cases_eva = DATA_USERS[USER_NAME]['type_evaluates']
-	sorted_types = list(map(lambda x: CASE_TYPES.index(x), sorted(cases_eva.keys(), key=lambda x: cases_eva[x])))
+	type_info = DATA_USERS[USER_NAME]['type_info']
+	sorted_types = list(map(lambda x: CASE_TYPES.index(x), sorted(type_info.keys(), key=lambda x: type_info[x]['rank'])))
 	if sorted_types[0] != type:
 		rec_type = sorted_types[0]
 	else:
 		rec_type = sorted_types[1]
-	cases = getRecommendCase(type, getOffset(cases_eva[CASE_TYPES[type]])) + getRecommendCase(rec_type, getOffset(cases_eva[CASE_TYPES[rec_type]]))
+	cases = getRecommendCase(type, getOffset(type)) + getRecommendCase(rec_type, getOffset(rec_type))
 	
 	print("系统为您推荐以下练习题目：")
 	for i in range(len(cases)):
@@ -135,17 +137,27 @@ def exercise():
 	pass
 
 
+def getEvaluate():
+	print(" 用户名："+USER_NAME)
+	print(" 编程分数："+str(DATA_USERS[USER_NAME]['rank_score']))
+	stdEva.getAbilityFigure(DATA_USERS[USER_NAME]['type_info'])
+
+
 def start():
 	print("成功进入系统!")
-	print("请选择进行测试或者练习（测试可以提高用户的编程评估分，从而有可能被推送到难度更高的题目，练习则不影响评估分)")
-	flag = input("（回复‘E’表示练习，‘T’表示测试）：")
-	while flag != 'T' and flag != 'E':
-		flag = input("请输入‘T’或者‘E’")
-	if flag == 'T':
-		test()
-	else:
-		exercise()
-
+	print("进行测试可以提高用户的编程评估分，从而有可能被推送到难度更高的题目，进行练习则不影响评估分。你也可以选择查看你的能力评估")
+	flag = input("请输入你的指令（回复‘E’表示练习，‘T’表示测试，‘D’表示查看能力评估，‘Q’表示退出）：")
+	while flag != 'T' and flag != 'E' and flag != 'D' and flag != 'Q':
+		flag = input("请输入‘T’或者‘E’或者‘D’或者‘Q’")
+	while flag != 'Q':
+		if flag == 'T':
+			test()
+		elif flag == 'E':
+			exercise()
+		elif flag == 'D':
+			getEvaluate()
+		flag = input("请输入你的指令（回复‘E’表示练习，‘T’表示测试，‘D’表示查看能力评估，‘Q’表示退出）：")
+		
 
 if __name__ == '__main__':
 	DATA_USERS = getUserData()
